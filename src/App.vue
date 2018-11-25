39a8462c4d97664b1479bb882798129f9245aabb
mangdehuang <1032036512@qq.com>
2018-11-25 11:55:24 +0800

    header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+var html = document.documentElement;
+var width = html.getBoundingClientRect().widht;
+html.style.fontSize = width / 15 + "px";
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='scss'>
+@import "assets/icon/iconfont.css";
+@import "assets/style/global.scss";
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="Header">
+        <div class="search-scan">
+            <i class="icon iconfont icon-saoyisao"></i>
+        </div>
+        <div class="search-input" >
+          <div class="search-btn"></div>
+          <input type="text" v-model="searchVal">
+        </div>
+        <div class="message">
+          <i class="icon iconfont icon-duihuakuang "></i>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      searchVal: "搜索所有联想产品"
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import "@/assets/style/global.scss";
+.Header {
+  background: red;
+  .icon.iconfont {
+    font-size: 1.666667rem;
+    color: #fff;
+  }
+}
+</style>
+    
